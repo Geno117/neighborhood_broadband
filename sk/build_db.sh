2040df67dff04a284fa278e3da385eb2fe81eea0
Eugene Meng <eugenemeng@cs.uchicago.edu>
2021-06-21 09:29:09 -0500

    change script for 2019

--- sk/build_db.sh
+++ sk/build_db.sh
@@ -2,27 +2,26 @@
 
 # set paswword in ~/.pgpass
 
-sk_psql="psql -d sk -U jsaxon -p 5432 -h localhost -f "
+sk_psql="psql -d sk -U geno -f "
 
 
 $sk_psql sql/schema
 
-for ym in $(ls 20180[1-3].tar.gz | sed "s/.tar.gz//"); do 
+# for ym in $(ls 20180[1-3].tar.gz | sed "s/.tar.gz//"); do 
 
-  echo $ym
+#   echo $ym
 
-  cp $ym.tar.gz current_archive.tar.gz
-  tar xzvf current_archive.tar.gz
+#   cp $ym.tar.gz current_archive.tar.gz
+#   tar xzvf current_archive.tar.gz
 
-  # postgres variables are messy with
-  #  script + \copy 
-  mv $ym curr_data/
-  $sk_psql sql/copy 
+#   # postgres variables are messy with
+#   #  script + \copy 
+  # mv $ym curr_data/
+$sk_psql sql/copy 
 
   # rm current_archive.tar.gz
-  mv curr_data $ym
+# mv curr_data $ym
 
 done
 
 # $sk_psql sql/finalize
-
